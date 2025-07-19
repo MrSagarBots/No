7fc053f206a58b210667c484089507399f4db4dd
Mr Sagar <143385461+MrSagarBots@users.noreply.github.com>
2025-07-19 10:57:36 +0530

    Create netflix.js

---
+++ api/netflix.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  const { query } = req.query;
+
+  if (!query) {
+    res.status(400).send("Missing 'query' parameter");
+    return;
+  }
+
+  const redirectUrl = `https://www.netflix.com/search?q=${encodeURIComponent(query)}`;
+  res.writeHead(302, { Location: redirectUrl });
+  res.end();
+}
